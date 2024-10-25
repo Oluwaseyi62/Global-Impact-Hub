82da1ad178af6d73cd075113b092924bab65ec46
Oluwaseyi62 <oluwaseyiayoade62@gmail.com>
2024-10-25 20:13:55 +0100

    Volunteers

--- assets/js/main.js
+++ assets/js/main.js
@@ -253,16 +253,7 @@
 
 				});
 
-				document.getElementById("contactForm").addEventListener("submit", function (event) {
-					const name = document.getElementById("name").value;
-					const email = document.getElementById("email").value;
-				
-					if (name === "" || email === "") {
-						alert("Please fill in all fields.");
-						event.preventDefault(); // Stop form from submitting
-					}
-				});
-				
+			
 
 			});
 
